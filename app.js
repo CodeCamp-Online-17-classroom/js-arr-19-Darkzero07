c6a465f5f1b4c28755a3310cfeb5177327a4f98a
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-02 21:51:30 +0700

    js lab 105

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+function checkEmptyObj(obj) {
+  return JSON.stringify(obj) === "{}";
+}
+
+console.log(checkEmptyObj({})); // true
+console.log(checkEmptyObj({ name: "John", age: 30 })); // false
+console.log(checkEmptyObj(null)); // false
+console.log(checkEmptyObj([])); // false
+console.log(checkEmptyObj(123)); // false
